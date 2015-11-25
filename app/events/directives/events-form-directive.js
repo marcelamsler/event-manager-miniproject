5ecd704b13498d834a3a283cf8b85f5657ed134b
Marcel Amsler <marcelamsler@Marcels-MacBook-Pro.local>
2015-11-25 09:43:04 +0100

    removed controller for form

--- app/events/directives/events-form-directive.js
+++ app/events/directives/events-form-directive.js
@@ -3,18 +3,6 @@ angular.module('eventManagerApp.events')
     .directive('eventsFormDirective', function () {
         return {
             templateUrl: 'events/templates/form.html',
-            scope: false,
-            controller: "EventsFormCtrl"
+            scope: false
         };
-    })
-
-    .controller('EventsFormCtrl', ['$scope', 'eventsService', '$location', function ($scope, eventsService, $location) {
-        $scope.save = function () {
-            eventsService.saveEvent($scope.event).then(function () {
-                eventsService.loadAllEvents().then(function () {
-                    $location.path('/events');
-                });
-            });
-
-        };
-    }]);
+    });
