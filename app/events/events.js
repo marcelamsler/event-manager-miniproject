82a32afd8d2ceb9d7773aad682005430773f1ac7
Philipp Christen <philipp.christen@gmail.com>
2015-11-26 21:57:07 +0100

    different mechanism on small screens

--- app/events/events.js
+++ app/events/events.js
@@ -29,4 +29,14 @@ angular.module('eventManagerApp.events', ['ngRoute'])
             return event.maximalAmountOfGuests - event.guests.length > 0;
         };
 
+        /**
+         * Set a certain event active (or inactive again) and set all other
+         * events inactive.
+         */
+        $scope.setActive = function( event ) {
+          angular.forEach($scope.events, function(ev){
+            ev.activeInList = ev.id === event.id && !event.activeInList;
+          });
+        }
+
     }]);
